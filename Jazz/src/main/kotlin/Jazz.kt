7c9466dbde1799945042151738c98327f9b5bee4
Arthur Lui <luiarthur@gmail.com>
2017-08-21 21:27:56 -0700

    yes

--- Jazz/src/main/kotlin/Jazz.kt
+++ Jazz/src/main/kotlin/Jazz.kt
@@ -2,58 +2,68 @@ object Jazz {
 
   val letterNames = listOf('A'..'G').flatten()
   val octaves = listOf(1..7).flatten()
-  val accidentals = listOf("#", "b", "x", "d", "n", "")
+  val accidentals = listOf("#", "x", "b", "bb", "n", "")
+
   val lowestNote = "A0"
   val HighestNote = "C8"
   val sharpableNotes = listOf('C', 'D', 'F', 'G', 'A')
 
-  // FIXME
-  fun generateNotes(pianoNotes: List<String> = listOf(lowestNote, "A0#")): List<String> {
-    val currentNote = pianoNotes.last()
-    val currentLetter = currentNote[0]
-    val currentOctave = currentNote[1].toString().toInt()
-    val currentSharped = currentNote.drop(2) == "#"
-    val currentLetterIdx = currentNote.indexOf(currentLetter)
-
-    if (currentNote == HighestNote) {
-      return pianoNotes
-    } else {
-      val nextLetter = letterNames[if (currentLetterIdx + 1 == letterNames.size) 0 else currentLetterIdx + 1]
-      val nextOctave = (if (currentLetter == 'B') currentOctave + 1 else currentOctave).toString()
-      val nextAccidental = if (!currentSharped && sharpableNotes.contains(currentLetter)) "#" else ""
-
-      val nextNote = nextLetter + nextOctave + nextAccidental
-      return pianoNotes + nextNote
-    }
-  }
-
-  val pianoNotes = generateNotes()
+  val pianoNotes = listOf(
+      "A0", "A0#", "B0", 
+      "C1", "C1#", "D1", "D1#", "E1", "F1", "F1#", "G1", "G1#", "A1", "A1#", "B1",
+      "C2", "C2#", "D2", "D2#", "E2", "F2", "F2#", "G2", "G2#", "A2", "A2#", "B2",
+      "C3", "C3#", "D3", "D3#", "E3", "F3", "F3#", "G3", "G3#", "A3", "A3#", "B3",
+      "C4", "C4#", "D4", "D4#", "E4", "F4", "F4#", "G4", "G4#", "A4", "A4#", "B4",
+      "C5", "C5#", "D5", "D5#", "E5", "F5", "F5#", "G5", "G5#", "A5", "A5#", "B5",
+      "C6", "C6#", "D6", "D6#", "E6", "F6", "F6#", "G6", "G6#", "A6", "A6#", "B6",
+      "C7", "C7#", "D7", "D7#", "E7", "F7", "F7#", "G7", "G7#", "A7", "A7#", "B7",
+      "C8")
 
   // Note class
-  class Note(val note: String) {
-    override fun toString(): String { return note }
+  class Note(val letter: Char, val octave: Int, val accidental: String="") {
+    override fun toString(): String {
+      return letter.toString() + octave + accidental
+    }
 
-    fun isEnharmonic(): Boolean {
-      TODO()
+    constructor(s: String) : this(s[0], s[1].toString().toInt(), s.drop(2))
+
+    fun toEnharmonic(): Note? { // Note with sharp
+      val halfSteps = when (accidental) {
+        "#"  ->  1
+        "x"  ->  2
+        "b"  -> -1
+        "bb" -> -2
+        else ->  0 // "n" or ""
+      }
+
+      val currentIdx = pianoNotes.indexOf(letter.toString() + octave)
+      val enharmonicIdx = currentIdx + halfSteps
+
+      val note = if (currentIdx > -1 && 0 <= enharmonicIdx && enharmonicIdx < 88) {
+        val enharmonic= pianoNotes[enharmonicIdx]
+        val eLetter = enharmonic[0]
+        val eOctave = enharmonic[1].toString().toInt()
+        val eAccidental = enharmonic.drop(2)
+
+        Note(eLetter, eOctave, eAccidental)
+      } else null
+
+      return note
     }
 
     fun isValid(): Boolean {
-      val letterName = note[0]
-      val octave = note[1].toString().toInt()
-      val accidental = note.drop(2)
-
-      val valid = letterNames.contains(letterName) &&
-                  accidentals.contains(accidental) &&
-                  octaves.contains(octave)
-
-      return valid
+      return when (this.toEnharmonic()) {
+        null -> false
+        else -> true
+      }
     }
+
 
     fun transpose(halfSteps: Int): Note { 
       require(0 < halfSteps && halfSteps < 12) {
         "halfSteps must be between 0 and 12 (exclusive)."
       }
-      TODO() 
+      return Note(pianoNotes[pianoNotes.indexOf(this.toEnharmonic().toString()) + halfSteps])
     }
   }
 
@@ -64,7 +74,7 @@ object Jazz {
     }
 
     fun transpose(halfSteps: Int): List<Note> {
-      return notes.map{ Note(it.transpose(halfSteps).note) }
+      return notes.map{ it.transpose(halfSteps) }
     }
 
     fun circle(): List<String> {
@@ -80,7 +90,7 @@ object Jazz {
   }
 
 
-  // Chord class
+  //// Chord class
   class Chord(override val notes: List<Note>, val key: Key) : ListNotes(notes) { 
   }
 
@@ -90,6 +100,5 @@ object Jazz {
       TODO()
     }
   }
-
 }
 
--- Jazz/src/test/kotlin/test.kt
+++ Jazz/src/test/kotlin/test.kt
@@ -7,7 +7,7 @@ class TestSource {
         assertEquals(1.toString() + "1", "11")
     }
 
-    @Test fun `Test Jazz Notes are Correct`() {
+    @Test fun `Test Jazz Piano Notes are Correct`() {
       assert(Jazz.pianoNotes[0] == "A0")
       assert(Jazz.pianoNotes[1] == "A0#")
       assert(Jazz.pianoNotes[2] == "B0")
@@ -16,11 +16,23 @@ class TestSource {
       assert(Jazz.pianoNotes.size == 88)
     }
 
-    @Test fun `Test Jazz`() {
+    @Test fun `Test Jazz Note`() {
       println("Hi")
+      assert( Jazz.Note('A', 3, "#").isValid() )
+      assert( Jazz.Note('A', 3, "#").toString() == "A3#" )
+      assert( Jazz.Note('B', 3, "b").isValid() )
+      assert( Jazz.Note('B', 3, "b").toEnharmonic().toString() == "A3#" )
+      assert( Jazz.Note('B', 3, "b").toEnharmonic().toString() == 
+              Jazz.Note('A', 3, "#").toEnharmonic().toString())
+      assert( !Jazz.Note('C', 8, "#").isValid() )
+      assert( !Jazz.Note('A', 9).isValid() )
+      assert( !Jazz.Note('F', 8, "#").isValid() )
+      assert( Jazz.Note('A', 0, "#").isValid() )
+      assert( !Jazz.Note('A', 0, "b").isValid() )
+      assert( !Jazz.Note('G', 0, "x").isValid() )
+      assert( !Jazz.Note('D', 8, "bb").isValid() )
+      assert( !Jazz.Note("D8bb").isValid() )
       assert( Jazz.Note("A3#").isValid() )
-      assert( Jazz.Note("A3#").toString() == "A3#" )
-      assert( !Jazz.Note("A9").isValid() )
     }
 
 }

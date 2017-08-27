d50cc6d20571bf47cda25151d08268621edeb56d
Arthur Lui <luiarthur@gmail.com>
2017-08-27 08:49:58 -0700

    yes

--- jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/MainActivity.kt
+++ jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/MainActivity.kt
@@ -2,11 +2,15 @@ package luiarthur.github.io.jazzscales
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
+
+    val url = "file:///android_asset/html/template.html"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +21,8 @@ class MainActivity : AppCompatActivity() {
         //val webview = WebView(this)
         val webview = findViewById<WebView>(R.id.webViewScore)
         //setContentView(webview)
-        val webSettings = webview.getSettings()
-        webSettings.setJavaScriptEnabled(true)
+        val webSettings = webview.settings
+        webSettings.javaScriptEnabled = true
 
         // Method 1
         //val summary = "<html><body>You scored <b>192</b> points.</body></html>"
@@ -28,12 +32,34 @@ class MainActivity : AppCompatActivity() {
         // Load url: This file resides in app/src/assets/html/template.html.
         // (Notice the "s" in assets)
         webview.setWebViewClient(WebViewClient()) // opens in activity, instead of new window
-        val html = "file:///android_asset/html/template.html"
-        webview.loadUrl(html)
+        webview.loadUrl(url)
+        // Trying to change the music. Not Working???
+        //webview.evaluateJavascript("""
+        //  var music = `
+        //    %%staffwidth 400
+        //    L:1/1
+        //    [DE][DF][EG]
+        //    `;
+        //  ABCJS.renderAbc('music',music)}""", null)
 
         // Method 3 (can't display js?)
-        //val html:String = getAssets().open("html/template.html").bufferedReader().use { it.readText() }.replace("\n","")
+        //val html:String = assets.open("html/template.html").bufferedReader().use { it.readText() }.replace("\n","")
         //webview.loadData(html, "text/html", null)
+
+    }
+
+    fun refreshWebViewScore(v: View) {
+        val webview = findViewById<WebView>(R.id.webViewScore)
+        webview.evaluateJavascript("""
+          var newMusic = `
+            %%staffwidth 400
+            L:1/1
+            [DE][DF][EG] |
+            `;
+          ABCJS.renderAbc('music',newMusic)""", null)
+
+        val tv = findViewById<TextView>(R.id.textView)
+        tv.text = "Here's new music!"
     }
 }
 

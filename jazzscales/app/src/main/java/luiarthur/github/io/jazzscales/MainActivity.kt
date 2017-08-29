0e0106c89938323a929056cfc014ecfb73469aa2
Arthur Lui <luiarthur@gmail.com>
2017-08-28 21:16:05 -0700

    yes

--- jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/MainActivity.kt
+++ jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/MainActivity.kt
@@ -6,60 +6,70 @@ import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     val url = "file:///android_asset/html/template.html"
+    private var musicStaffWidth = 400
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         // Enable JavaScript calling from html files
-        //val webview = WebView(this)
-        val webview = findViewById<WebView>(R.id.webViewScore)
-        //setContentView(webview)
-        val webSettings = webview.settings
-        webSettings.javaScriptEnabled = true
+        webViewScore.settings.javaScriptEnabled = true
 
         // Method 1
         //val summary = "<html><body>You scored <b>192</b> points.</body></html>"
         //webview.loadData(summary, "text/html", null)
 
-        // Method 2
+        // Method 2 (can't display js?)
         // Load url: This file resides in app/src/assets/html/template.html.
         // (Notice the "s" in assets)
-        webview.setWebViewClient(WebViewClient()) // opens in activity, instead of new window
-        webview.loadUrl(url)
-        // Trying to change the music. Not Working???
-        //webview.evaluateJavascript("""
-        //  var music = `
-        //    %%staffwidth 400
-        //    L:1/1
-        //    [DE][DF][EG]
-        //    `;
-        //  ABCJS.renderAbc('music',music)}""", null)
-
-        // Method 3 (can't display js?)
         //val html:String = assets.open("html/template.html").bufferedReader().use { it.readText() }.replace("\n","")
         //webview.loadData(html, "text/html", null)
 
+        // Method 3
+        webViewScore.webViewClient = WebViewClient() // opens in activity, instead of new window
+        webViewScore.loadUrl(url)
     }
 
-    fun refreshWebViewScore(v: View) {
-        val webview = findViewById<WebView>(R.id.webViewScore)
-        webview.evaluateJavascript("""
-          var newMusic = `
-            %%staffwidth 400
-            L:1/1
-            [DE][DF][EG] |
-            `;
-          ABCJS.renderAbc('music',newMusic)""", null)
+    fun renderMusic(music:String) {
+        // sharps: '^' preceding note
+        // flats:  '_' preceding note
+        webViewScore.evaluateJavascript( "var music = `%%staffwidth $musicStaffWidth\n $music`", null)
+        webViewScore.evaluateJavascript("ABCJS.renderAbc('music', music)", null)
+    }
 
-        val tv = findViewById<TextView>(R.id.textView)
-        tv.text = "Here's new music!"
+    fun setStaffWidth(staffWidth: Int) {
+        musicStaffWidth = staffWidth
+    }
+
+    val CmajDim6 = "CDEFG^GABc"
+
+    fun transpose(music: String): String{
+        // music must be in key of C
+        // change the Key (K:C)
+        // change the music
+        TODO()
+    }
+
+    fun refreshWebViewScore(v:View) {
+        //renderMusic("""
+        //    L: 1/1
+        //    K: C
+        //    [V:1 clef=treble] [DG][FC][E^A] |
+        //    [V:2 clef=bass]   [E,B,][D,A,][C,G,] |
+        //""")
+        renderMusic("""
+          L:1/1
+          K:C
+          $CmajDim6
+        """)
+
+        textView.text = "Here's new music!"
     }
 }
 

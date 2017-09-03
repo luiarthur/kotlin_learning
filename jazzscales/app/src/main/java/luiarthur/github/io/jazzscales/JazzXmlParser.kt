ed88c909ac8ee5a25ff6c16bb9c9742b6bab3e84
Arthur Lui <luiarthur@gmail.com>
2017-09-02 20:16:25 -0700

    yes

--- jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/JazzXmlParser.kt
+++ jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/JazzXmlParser.kt
@@ -5,9 +5,9 @@ import kotlin.text.Regex
 object JazzXmlParser {
   class Xml(val xml: String) {
     override fun toString(): String = xml
-    val tag = Regex("<\\w+").find(xml)?.value?.replace("<", "")
-    val name = Regex("name[^>]*>").find(xml)?.value?.dropLast(1)?.replace("name=","")
-    val value = Regex(">[^<]*<").find(xml)?.value?.drop(1)?.dropLast(1)
+    val tag = Regex("<\\w+").find(xml)!!.value.replace("<", "") 
+    val name = Regex("name[^>]*>").find(xml)!!.value.dropLast(1).replace("name=","").replace("\"", "")
+    val value = Regex(">[^<]*<").find(xml)!!.value.drop(1).dropLast(1)
   }
 
   fun getAllTags(xml: String, tag: String): List<String> {
@@ -18,25 +18,29 @@ object JazzXmlParser {
 }  
 
 
-// Tests
-//  val testXml = """
-//  <?xml version="1.0" encoding="utf-8"?>
-//  <resources>
-//      <!-- predefined scales-->
-//      <scale name="Cdim6">CDEFG^GABc
-//      </scale>
-//      <scale name="C">CDEFGABc</scale>
-//
-//      <!-- predefined chords-->
-//      <chord name="C">[EGBD]</chord>
-//
-//      <!-- predefined custom patterns-->
-//      <custom name="custom1">[C E] D</custom>
-//  </resources>
-//  """
+/* Tests
+:load JazzXmlParser.kt
+val testXml = """
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <!-- predefined scales-->
+    <scale name="Cdim6">CDEFG^GABc
+    </scale>
+    <scale name="C">CDEFGABc</scale>
 
-// val scales = getAllTags(testXml, "scale")
-// val s = scales.map{Xml(it)}
-// s[1].tag
-// s[1].name
-// s[1].value
+    <!-- predefined chords-->
+    <chord name="C">[EGBD]</chord>
+
+    <!-- predefined custom patterns-->
+    <custom name="custom1">[C E] D</custom>
+</resources>
+"""
+
+val scales = JazzXmlParser.getAllTags(testXml, "scale")
+val s = scales.map{JazzXmlParser.Xml(it)}
+s[1].tag
+s[1].name
+s[1].value
+s.map{it.name}
+s.filter{it.name == "Cdim6"}.first().value
+*/
--- jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/MainActivity.kt
+++ jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.webkit.WebViewClient
 import android.widget.PopupMenu
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
 
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     val url = "file:///android_asset/html/template.html"
     private var musicStaffWidth = 400
+    private var collections: String = ""
+    private var lists: String = ""
 
     //val collections = assets.open("jazz/collections.html")//.bufferedReader().use { it.readText() }.replace("\n","")
     //private val scales = JazzXmlParser.getAllTags(collections, "scale").map{JazzXmlParser.Xml(it)}
@@ -35,9 +38,13 @@ class MainActivity : AppCompatActivity() {
         // Method 2
         wvScore.webViewClient = WebViewClient() // opens in activity, instead of new window
         wvScore.loadUrl(url)
+
+        // Initialize jazz collections and lists
+        collections = readTxt("jazz/collections.xml")
+        lists = readTxt("jazz/lists.xml")
     }
 
-    fun renderMusic(music:String) {
+    fun renderMusic(music: String) {
         // sharps: '^' preceding note
         // flats:  '_' preceding note
         wvScore.evaluateJavascript( "var music = `%%staffwidth $musicStaffWidth\n $music`", null)
@@ -47,9 +54,6 @@ class MainActivity : AppCompatActivity() {
     fun setStaffWidth(staffWidth: Int) {
         musicStaffWidth = staffWidth
     }
-
-
-
 
     fun transpose(music: String): String{
         // music must be in key of C
@@ -66,13 +70,14 @@ class MainActivity : AppCompatActivity() {
         //    [V:2 clef=bass]   [E,B,][D,A,][C,G,] |
         //""")
 
+        val scales = JazzXmlParser.getAllTags(collections, "scale").map{JazzXmlParser.Xml(it)}
+        val Cdim6:String = scales.filter{it.name=="Cdim6"}.first().value!!
+
         renderMusic("""
           L:1/1
           K:C
-          ABC
+          $Cdim6
         """)
-
-        //listAssetFiles("jazz")
     }
 
     fun expandLists(v:View) {
@@ -84,6 +89,25 @@ class MainActivity : AppCompatActivity() {
     //fun doSomething(v: View) {
     //   TODO()
     //}
+
+    // read text file relative to assets dir
+    private fun readTxt(path: String):String {
+        val ins = assets.open(path)
+        val baos = ByteArrayOutputStream()
+        var i: Int
+        try {
+            i = ins.read()
+            while (i > -1) {
+                baos.write(i)
+                i = ins.read()
+            }
+            ins.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return baos.toString()
+    }
 }
 
 

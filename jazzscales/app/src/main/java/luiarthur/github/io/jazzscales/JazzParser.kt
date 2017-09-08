74ee215ce3ed02b238ad2778521a684bfd5da193
Arthur Lui <luiarthur@gmail.com>
2017-09-08 10:35:40 -0700

    yes

--- jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/JazzParser.kt
+++ jazzscales/app/src/main/java/luiarthur/github/io/jazzscales/JazzParser.kt
@@ -2,44 +2,86 @@ package luiarthur.github.io.jazzscales
 
 import kotlin.text.Regex
 
-data class JazzData(type: String, name: String, music: String, list: String="none")
+data class JazzData(
+    val type: String, 
+    val name: String, 
+    val list: List<String>,
+    val music: String) {
+  override fun toString(): String {
+    return "type:$type; name:$name; list:${list.joinToString()}, music:{$music};"
+  }
+}
 
+class Parser(val text: String) {
+  override fun toString(): String {
+    return text
+  }
+
+  val re = Regex("type\\s*:\\s*\\w+\\s*;\\s*name\\s*:\\s*\\w+\\s*;\\s*list\\s*:(\\s*\\w+\\s*,\\s*)*\\w+\\s*;\\s*music\\s*:\\s*\\{[^\\}]+};")
+
+  private fun lineToJazzData(line: String): JazzData {
+    val tnlm = line.split(";").map{it.split(":").last().trim()}
+    return JazzData(tnlm[0], tnlm[1], tnlm[2].split(",").map{it.trim()}, tnlm[3].drop(1).dropLast(1))
+  }
+
+  fun toJazzData(): List<JazzData> {
+    val ls = re.findAll(text).map{it.value}.toList()
+    return ls.map{line -> lineToJazzData(line)}
+  }
+
+  val jazzData = this.toJazzData()
+
+  fun getAll(type: String): List<JazzData> {
+    require(type in listOf("scale","chord","custom"))
+    return jazzData.filter{ it.type == type }
+  }
+
+  fun addElement(type:String, name: String, list: List<String>, music: String): Parser {
+    require(type in listOf("scale","chord","custom"))
+    return Parser(text + "\n" + JazzData(type,name,list,music).toString())
+  }
+
+  fun rmElement(name: String, music: String, list: String): Parser {
+    TODO()
+  }
+
+  fun editElement(name: String, music: String, list: String): Parser {
+    TODO()
+  }
+}
 
 /* Tests
 :load JazzParser.kt
 val text = """
 ### predefined scales ###
-type:scale; name:Cdim6; music:CDEFG^GABc; list:favorites,routines;
-type:scale; name:C; music:CDEFGABc; list:routines;
+type:  scale; name: Cdim6; list: favorites  , routines; music: {
+  CDEFG^GABc
+};
+type :scale; name:C; list:routines; music:{CDEFGABc}; 
 
 ### predefined chords ###
-type:chord; name:C; music:[EGBD]; list:favorites;
+type:chord; name:C; list:favorites; music:{[EGBD]}; 
 
 ### predefined custom patterns ###
-type:custom; name:custom1; music:[C E] D; list:none;
+type:custom; name:custom1; list:none; music:{[C E] | D}; 
 
-type:custom;
-name:custom1;
-music:[C E]
-FGA;
-list:none;
+type:custom; name:custom1; list:none; music:{
+  [C E] |
+  FGA
+};
 
 ### user-define items ###
 """
 
-val scales = JazzXmlParser.getAllTags(testXml, "scale")
-val s = scales.map{JazzXmlParser.Xml(it)}
-s[1].tag
-s[1].name
-s[1].value
-s.map{it.name}
-s.filter{it.name == "Cdim6"}.first().value
+val p = Parser(text)
+val j = p.toJazzData()
+j.forEach{println(it)}
 
-JazzXmlParser.Xml(JazzXmlParser.findElement(testXml, "Cdim6", "scale")).value
+p.getAll("scale")
+p.getAll("chord")
+p.getAll("custom")
 
-val newElement = """<scale name="D">DDDDD</scale>"""
-JazzXmlParser.appendElement(testXml, newElement)
-JazzXmlParser.removeElement(testXml, "C", "scale")
-JazzXmlParser.findElement(testXml, "C", "scale")
-JazzXmlParser.editElement(testXml, "C", "scale", "ABC")
+val p1 = p.addElement("scale", "bebop" , listOf("none"), "CDEFGA_BBc")
+p1.addElement("scale", "bebop" , listOf("none"), "CDEFGA_BBc")
+
 */

fbd1933c2577b29c1a8c3eabe74eb4105b8d9ce7
Arthur Lui <luiarthur@gmail.com>
2017-09-19 21:07:50 -0700

    yes

--- jsrender/js/jazz_interface.js
+++ jsrender/js/jazz_interface.js
@@ -3,6 +3,10 @@
  *   - abcjs_basic_3.0-min.js
  */
 
+// Global Variables Dictionary 
+global_vars = {'path_to_jazz_orig': '/assets/jazzData_orig.txt',
+               'path_to_user_jazz': '/assets/userCreated/jazzData.txt'}
+
 function fillTextArea(music) {
   $('#edit').val(music);
 }
@@ -26,6 +30,27 @@ function renderMusic() {
   console.log(music)
   ABCJS.renderAbc('music', music);
 }
+
+function readFile(path=global_vars.path_to_jazz_orig, var_name='fileContent') {
+  $.ajax({ url: path, success: function(file_content) {
+      global_vars[var_name] = file_content;
+    }
+  });
+}
+
+function writeFile(path=global_vars.path_to_user_jazz, var_name='fileContent') {
+  $.ajax({ url: path, success: function(file_content) {
+      global_vars[var_name] = file_content;
+    }
+  });
+}
+
+function showExample() {
+  readFile();
+  fillTextArea(global_vars.fileContent);
+}
+
+
 /*  500: 695
  *  600: 828
  *  700: 961

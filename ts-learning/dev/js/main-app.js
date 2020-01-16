606efd93456182d376010f81aaa81584302259e3
Arthur Lui <luiarthur@gmail.com>
2020-01-15 21:14:17 -0700

    yes

--- ts-learning/dev/js/main-app.js
+++ ts-learning/dev/js/main-app.js
@@ -25,8 +25,12 @@ define("file1", ["require", "exports", "file2"], function (require, exports, F2)
 define("main", ["require", "exports", "file1", "file2"], function (require, exports, F1, F2) {
     "use strict";
     exports.__esModule = true;
-    var x = 1;
-    F1.println("Hey there");
-    console.log('boo');
-    F1.println(F2.doubleMe(3));
+    function main() {
+        var x = 1;
+        F1.println("Hey there");
+        console.log('boo');
+        F1.println(F2.doubleMe(3));
+        return x;
+    }
+    exports.main = main;
 });
